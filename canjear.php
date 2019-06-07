57327df92375eba57d65d1a8d492d1f31db0fa0a
Andrea Diaz <dima0830@gmail.com>
2019-06-06 22:40:44 -0500

    primeros archivos

---
+++ canjear.php
@@ -0,0 +1,290 @@
+<section>
+	<div class="banner canje mp pc">
+		<img src="assets/img/canjear.png" class="img-fluid iconb">
+		<p class="tit-b">CANJEAR ESTRELLAS</p>
+	</div>
+	<div class="banner canjem mobil md">
+		<img src="assets/img/canjear.png" class="img-fluid iconb">
+		<p class="tit-b">CANJEAR ESTRELLAS</p>
+	</div>
+</section>
+<div class="container">
+	<section>
+		<br>
+		<div class="row">
+			<div class="col-sm-4 col-lg-3">
+				<div class="box-peole-g">
+					<br>
+					<button type="button" class="btn bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg">
+						<div class="circular--square">
+							<img  src="assets/img/avatar.jpg" />
+						</div>
+					</button>
+					<p class="nombre">John Doe</p>
+					<p class="cargo">Diseñador Gráfico</p>
+					<p class="puesto">Artes gráficas</p>
+					<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+					<hr class="hr-box">
+					<p class="otorgar-p">
+						<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+							Otorgar Estrellas 
+						</button>
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-8 col-lg-9">
+				<h5 class="box-titulo sinm">RECONOCIMIENTO</h5>
+				<h1 class="titu1 otram">
+					2 ESTRELLAS
+				</h1>
+				<div class="box-princ">
+					<div class="row">
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<a href="canjear2" class="btn btn-primary">Canjear</a>
+								<div class="space"></div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+
+				<!--3 y 4 estrellas -->
+				<h1 class="titu1 otram">
+					3 y 4 ESTRELLAS
+				</h1>
+				<div class="box-princ">
+					<div class="row">
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<!--5 o más estrellas -->
+				<h1 class="titu1 otram">
+					5 ó MÁS ESTRELLAS
+				</h1>
+				<div class="box-princ">
+					<div class="row">
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="box-peole sinpad">
+								<div class="im-canj">
+									<img src="assets/img/img-canj.jpg" class="img-fluid">
+								</div>
+
+								<p class="nombre-sm">PREMIO # 1</p>
+								<p class="cargo-sm">Decoración de tu lugar de
+								trabajo</p>
+								<hr class="hr-box">
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<button type="button" class="btn btn-primary">Canjear</button>
+								<div class="space"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</section>
+</div>
+
+<br>
---
+++ otorgar.php
@@ -0,0 +1,362 @@
+<section>
+	<div class="banner otorgar mp pc">
+		<img src="assets/img/otorgar-1.png" class="img-fluid iconb">
+		<p class="tit-b">OTORGAR ESTRELLAS</p>
+	</div>
+	<div class="banner otorgarm mobil md">
+		<img src="assets/img/otorgar-1.png" class="img-fluid iconb">
+		<p class="tit-b">OTORGAR ESTRELLAS</p>
+	</div>
+</section>
+<div class="container">
+	<section style="margin: 15px;">
+		<h1 class="titu1">
+				Top 10
+			</h1>
+		<div class="box-princ">
+			<div class="container">
+				<div class="row">
+					<div class="MultiCarousel" data-items="1,3,5,5" data-slide="1" id="MultiCarousel"  data-interval="1000">
+						<div class="MultiCarousel-inner" >
+							<div class="item">
+								<div class="box-top">
+									<div class="circular--square-small">
+									<img  src="assets/img/avatar.jpg" />
+								</div>									
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="box-top">
+								<div class="circular--square-small">
+									<img  src="assets/img/avatar2.jpg" />
+								</div>
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="box-top">
+								<div class="circular--square-small">
+									<img  src="assets/img/avatar.jpg" />
+								</div>
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<div>
+								<div class="circular--square-small">
+									<img  src="assets/img/avatar2.jpg" />
+								</div>
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="box-top">
+								<div class="circular--square-small">
+									<img  src="assets/img/avatar.jpg" />
+								</div>
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="box-top">
+								<div class="circular--square-small">
+									<img  src="assets/img/avatar.jpg" />
+								</div>
+
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="box-top">
+								<div class="circular--square-small">
+									<img  src="assets/img/avatar.jpg" />
+								</div>
+
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="box-top">
+								<div class="circular--square-small">
+									<img  src="assets/img/avatar.jpg" />
+								</div>
+
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="box-top">
+								<div class="circular--square-small">
+									<img  src="assets/img/avatar.jpg" />
+								</div>
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="box-top">
+								<div class="circular--square-small">
+									<img  src="assets/img/avatar.jpg" />
+								</div>
+
+								<p class="nombre-sm">John Doe</p>
+								<p class="cargo-sm">Diseñador Gráfico</p>
+								<p class="puesto-sm">Artes gráficas</p>
+								<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+								<hr class="hr-box">
+								<p class="otorgar-p">
+									<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+										Otorgar Estrellas 
+									</button>
+								</p>
+							</div>
+						</div>
+					</div>
+					<button class="btn  leftLst"><i class="fas fa-angle-left"></i></button>
+					<button class="btn rightLst"><i class="fas fa-chevron-right"></i></button>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section>
+		<div class="busq">
+				<div class="row justify-content-between">
+					<div class="col-sm-5 col-md-4 form-group" style="padding: 0; margin: 0;">
+						<form>
+						<div class="input-group mb-3" style="padding: 0; margin: 0;">
+							<input type="text" class="form-control" placeholder="Lorem Ipsum" aria-label="Recipient's username" aria-describedby="basic-addon2">
+							<div class="input-group-append ">
+								<span class="input-group-text bcolor" id="basic-addon2"><i class="fas fa-search"></i></span>
+							</div>
+						</div>
+						</form>
+					</div>
+						<div class="col-sm-5 col-md-4 form-group" >
+						<select class="form-control float-rig">
+							<option>Categoría</option>
+							<option>Lider</option>
+							<option>Área</option>
+							<option>Cliente Interno</option>
+						</select>
+					</div>
+				</div>
+			
+		</div>
+	</section>
+
+	<section>
+		<div class="row people">
+			<div class="col-lg col-sm-4">
+				<div class="box-peole">
+					<br>
+					<button type="button" class="btn bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg">
+						<div class="circular--square">
+							<img  src="assets/img/avatar.jpg" />
+						</div>
+					</button>
+					<p class="nombre">John Doe</p>
+					<p class="cargo">Diseñador Gráfico</p>
+					<p class="puesto">Artes gráficas</p>
+					<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+					<hr class="hr-box">
+					<p class="otorgar-p">
+						<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+							Otorgar Estrellas 
+						</button>
+					</p>
+				</div>
+			</div>
+			<div class="col-lg col-sm-4">
+				<div class="box-peole">
+					<br>
+					<button type="button" class="btn bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg">
+						<div class="circular--square">
+							<img  src="assets/img/avatar.jpg" />
+						</div>
+					</button>
+					<p class="nombre">John Doe</p>
+					<p class="cargo">Diseñador Gráfico</p>
+					<p class="puesto">Artes gráficas</p>
+					<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+					<hr class="hr-box">
+					<p class="otorgar-p">
+						<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+							Otorgar Estrellas 
+						</button>
+					</p>
+				</div>
+			</div>
+			<div class="col-lg col-sm-4">
+				<div class="box-peole">
+					<br>
+					<button type="button" class="btn bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg">
+						<div class="circular--square">
+							<img  src="assets/img/avatar.jpg" />
+						</div>
+					</button>
+					<p class="nombre">John Doe</p>
+					<p class="cargo">Diseñador Gráfico</p>
+					<p class="puesto">Artes gráficas</p>
+					<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+					<hr class="hr-box">
+					<p class="otorgar-p">
+						<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+							Otorgar Estrellas 
+						</button>
+					</p>
+				</div>
+			</div>
+			<div class="col-lg col-sm-4">
+				<div class="box-peole">
+					<br>
+					<button type="button" class="btn bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg">
+						<div class="circular--square">
+							<img  src="assets/img/avatar.jpg" />
+						</div>
+					</button>
+					<p class="nombre">John Doe</p>
+					<p class="cargo">Diseñador Gráfico</p>
+					<p class="puesto">Artes gráficas</p>
+					<div class="box-sm">
+									<spam class="star-box"><i class="fas fa-star"></i></spam>
+									<span class="cant-box">02</span>
+								</div>
+					<hr class="hr-box">
+					<p class="otorgar-p">
+						<button type="button" class="bt-foto" data-toggle="modal" data-target=".bd-example-modal-lg"> 
+							Otorgar Estrellas 
+						</button>
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div id="contenedor" class="load">
+		<div class="loader" id="loader">Loading...</div>
+	</div>
+
+</div>
+
+
